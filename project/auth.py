b306ea77fabf061980091cd71b6cb62624cec0aa
Teru Okada <okada.teru@gmail.com>
2019-11-03 17:09:24 +0800

    added logn POST

--- project/auth.py
+++ project/auth.py
@@ -6,6 +6,29 @@ from . import db
 
 auth = Blueprint('auth', __name__)
 
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    # Return the first result of this Query or None
+    user = User.query.filter_by(email=email).first()
+
+    # check if user actually exists
+    # take the user supplied password, hash it, and compare it to the hashed password in database
+    if not user or not check_password_hash(user.password, password): 
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login')) # if user doesn't exist or password is wrong, reload the page
+
+
+
+
+
+    return redirect(url_for('main.profile'))
+
 @auth.route('/login')
 def login():
     return render_template('login.html')
@@ -23,6 +46,11 @@ def signup_post():
         return redirect(url_for('auth.signup'))
 
     # create new user with the form data. Hash the password so plaintext version isn't saved.
+
+    # Hash a password with the given method and salt with a string of the given length.
+    # The format of the string returned includes the method that was used so that check_password_hash() can check the hash.
+
+    # return formt of generate_password_hash: method$salt$hash
     new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
 
     # add the new user to the database
